cec3f46b2c7bab10b16a3868af5e470243380769
RobersonDaza <robersondaza@gmail.com>
2017-09-28 19:22:56 -0500

    create sign in

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_divise_params, if: :devise_controller?
+
+      def configure_divise_params
+          devise_parameter_sanitizer.permit(:sign_up) do |user|
+              user.permit(:name, :email, :password, :password_confirmation)
+      end
+  end
+end
